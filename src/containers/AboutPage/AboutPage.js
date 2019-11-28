e146520b1bd5aa036f2e0b674c658e8d6b42d8b9
Allister Mannion <allister@oldencars.com>
2019-11-28 11:13:35 +0100

    Visual customization continued

--- src/containers/AboutPage/AboutPage.js
+++ src/containers/AboutPage/AboutPage.js
@@ -25,7 +25,7 @@ const AboutPage = () => {
       schema={{
         '@context': 'http://schema.org',
         '@type': 'AboutPage',
-        description: 'About Saunatime',
+        description: 'About OldenCars',
         name: 'About page',
       }}
     >

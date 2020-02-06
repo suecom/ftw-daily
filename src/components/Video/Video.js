f8e470e773fb2dec9d68a776be67b72a2cecbabf
Allister Mannion <allister@oldencars.com>
2020-02-06 22:41:52 +0000

    Added video section
    ade some css changes on the LandingPage.css

--- src/components/Video/Video.js
+++ src/components/Video/Video.js
@@ -9,11 +9,23 @@ const Video = props => {
   const { rootClassName, className } = props;
 
   const classes = classNames(rootClassName || css.root, className);
+
+  const opts = {
+    height: '100%',
+    width: '100%',
+    playerVars: { // https://developers.google.com/youtube/player_parameters
+      autoplay: 0
+    }
+  };
+
   return (
     <div className={classes}>
-      <YouTube
-        videoId="tFvW5rWgYUs"
-      />
+      <div className={css.video}>
+        <YouTube 
+          videoId="tFvW5rWgYUs"
+          opts={opts}
+        />
+      </div>
     </div>
   );
 };

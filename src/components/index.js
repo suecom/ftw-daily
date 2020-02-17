aeaa5df7bb447102aad278ded286f393bac1b204
Allister Mannion <allister@oldencars.com>
2020-02-17 15:13:44 +0000

    Fixed Payment Methid POST problem

--- src/components/index.js
+++ src/components/index.js
@@ -172,4 +172,3 @@ export { default as Topbar } from './Topbar/Topbar';
 export { default as TopbarDesktop } from './TopbarDesktop/TopbarDesktop';
 export { default as TopbarMobileMenu } from './TopbarMobileMenu/TopbarMobileMenu';
 export { default as TransactionPanel } from './TransactionPanel/TransactionPanel';
-export { default as Video } from './Video/Video';

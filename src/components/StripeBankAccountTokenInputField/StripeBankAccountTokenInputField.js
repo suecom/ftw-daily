829bd80eaf975d4cdb2c52a4d06fe23f6ccb76db
Allister Mannion <allister@oldencars.com>
2020-03-11 22:07:21 +0000

    Stripe updates

--- src/components/StripeBankAccountTokenInputField/StripeBankAccountTokenInputField.js
+++ src/components/StripeBankAccountTokenInputField/StripeBankAccountTokenInputField.js
@@ -132,7 +132,6 @@ class TokenInputFieldComponent extends Component {
     let accountData = {
       country,
       currency,
-      account_holder_type: 'individual',
     };
 
     // Include input values with correct stripe keys
